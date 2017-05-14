93e5a7908f8417761cf42a178d34f5bd7903fc41
ss910316 <ss910316@gmail.com>
2017-05-14 08:37:42 -0400

    2. green add desposit balance

--- bank_account.rb
+++ bank_account.rb
@@ -1,3 +1,13 @@
 class BankAccount 
-  
+ def initialize(amount)
+      @amount = amount
+    end
+
+    def balance
+      @amount
+    end
+
+    def deposit(amount)
+      @amount += amount
+    end
 end
--- bank_account_spec.rb
+++ bank_account_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe BankAccount do
     describe "存錢功能" do
       it "原本帳戶有 10 元，存入 5 元之後，帳戶餘額變 15 元" do
         account = BankAccount.new(10)
-        account = deposit 5
+        account.deposit 5
         expect(account.balance).to be 15
       end
 
