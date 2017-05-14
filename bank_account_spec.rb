3ee2591d62d399ea60aa7906c3d3cd59abec6fe1
ss910316 <ss910316@gmail.com>
2017-05-14 12:44:32 -0400

    3. red

--- bank_account_spec.rb
+++ bank_account_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe BankAccount do
       end
 
       it "原本帳戶有 10 元，存入 -5 元之後，帳戶餘額還是 10 元（不能存入小於等於零的金額）" do
+        account = BankAccount.new(10)
+        account.deposit -5
+        expect(account.balance).to be 10
       end
     end
 
